a8637c37f0dfa78e3f9fc6b4117fe259364cab02
Jorijn Schrijvershof <jorijn@jorijn.com>
2021-01-26 16:43:42 +0100

    add example for plain decoding without the denormalizers

---
+++ examples/decode_payment_request_into_array.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Jorijn\Bitcoin\Bolt11\Encoder\PaymentRequestDecoder;
+
+require dirname(__DIR__).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
+
+$invoice = 'lnbc9678785340p1pwmna7lpp5gc3xfm08u9qy06djf8dfflhugl6p7lgza6dsjxq454gxhj9t7a0sd8dgfkx7cmtwd68yetpd5s9xar0wfjn5gpc8qhrsdfq24f5ggrxdaezqsnvda3kkum5wfjkzmfqf3jkgem9wgsyuctwdus9xgrcyqcjcgpzgfskx6eqf9hzqnteypzxz7fzypfhg6trddjhygrcyqezcgpzfysywmm5ypxxjemgw3hxjmn8yptk7untd9hxwg3q2d6xjcmtv4ezq7pqxgsxzmnyyqcjqmt0wfjjq6t5v4khxxqyjw5qcqp2rzjq0gxwkzc8w6323m55m4jyxcjwmy7stt9hwkwe2qxmy8zpsgg7jcuwz87fcqqeuqqqyqqqqlgqqqqn3qq9qn07ytgrxxzad9hc4xt3mawjjt8znfv8xzscs7007v9gh9j569lencxa8xeujzkxs0uamak9aln6ez02uunw6rd2ht2sqe4hz8thcdagpleym0j';
+
+$decoder = new PaymentRequestDecoder();
+
+/**
+ * This example decodes the BOLT11 Payment Request into a plain PHP array. While this can be handy for explicit
+ * use cases I do recommend to use the packaged denormalizers. See the other examples how to use them.
+ */
+$paymentRequest = $decoder->decode($invoice);
+
+print_r($paymentRequest);
